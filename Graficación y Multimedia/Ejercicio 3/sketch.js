ce21f29bb0d4bb63f33359508649a47a3db83fe4
4KRO5 <4kro54k@gmail.com>
2024-03-12 15:46:57 -0700

    Colisión de pelotas

---
+++ Graficación y Multimedia/Ejercicio 3/sketch.js
@@ -0,0 +1,76 @@
+let circulos = [];
+
+function setup() {
+	createCanvas(500, 500);
+}
+
+function draw() {
+	background(220);
+
+	for (let i = 0; i < circulos.length; i++) {
+		circulos[i].mover();
+		circulos[i].mostrar();
+	}
+}
+
+function mousePressed() {
+	let c = new Circulo(mouseX, mouseY, 50);
+	circulos.push(c);
+}
+
+class Circulo {
+	constructor(x, y, d) {
+		this.x = x;
+		this.y = y;
+		this.d = d;
+		this.xSpeed = random(-3, 3);
+		this.ySpeed = random(-3, 3);
+	}
+
+	mover() {
+		if (this.x - this.d / 2 < 0 || this.x + this.d / 2 > width) {
+			this.xSpeed *= -1;
+		}
+		if (this.y - this.d / 2 < 0 || this.y + this.d / 2 > height) {
+			this.ySpeed *= -1;
+		}
+		this.x += this.xSpeed;
+		this.y += this.ySpeed;
+	}
+
+	mostrar() {
+		ellipse(this.x, this.y, this.d);
+	}
+
+	colisionar(otroCirculo) {
+		let dx = this.x - otroCirculo.x;
+		let dy = this.y - otroCirculo.y;
+		let distancia = sqrt(dx * dx + dy * dy);
+
+		if (distancia < this.d / 2 + otroCirculo.d / 2) {
+			let angulo = atan2(dy, dx);
+			let fuerza = 0.5;
+			let fuerzaX = cos(angulo) * fuerza;
+			let fuerzaY = sin(angulo) * fuerza;
+
+			this.xSpeed += fuerzaX;
+			this.ySpeed += fuerzaY;
+			otroCirculo.xSpeed -= fuerzaX;
+			otroCirculo.ySpeed -= fuerzaY;
+		}
+	}
+}
+
+function draw() {
+	background(220);
+
+	for (let i = 0; i < circulos.length; i++) {
+		for (let j = 0; j < circulos.length; j++) {
+			if (i != j) {
+				circulos[i].colisionar(circulos[j]);
+			}
+		}
+		circulos[i].mover();
+		circulos[i].mostrar();
+	}
+}  
