c00562156fc4b819bb6e593b9c7fc05e12b119b0
4KRO5 <4kro54k@gmail.com>
2024-04-04 19:43:04 -0700

    * algoritmo punto-pendiente

--- Graficación y Multimedia/ecuancion punto pendiente/sketch.js
+++ Graficación y Multimedia/ecuancion punto pendiente/sketch.js
@@ -4,18 +4,39 @@ function setup() {
 }
 
 function drawLine(x1, y1, x2, y2) {
-	let m = (y2 - y1) / (x2 - x1);
-	let b = y1 - m * x1;
+	let dx = x2 - x1;
+	let dy = y2 - y1;
 
-	for (let x = x1; x <= x2; x++) {
-		let y = m * x + b;
-		stroke(250, 0, 200);
-		point(x, y);
+	if (Math.abs(dx) < Math.abs(dy)) {
+		let steps = Math.abs(dy);
+		let xIncrement = dx / steps;
+		let yIncrement = dy / steps;
+		let x = x1;
+		let y = y1;
+
+		for (let i = 0; i <= steps; i++) {
+			point(x, y);
+			x += xIncrement;
+			y += yIncrement;
+		}
+	} else {
+		let steps = Math.abs(dx);
+		let xIncrement = dx / steps;
+		let yIncrement = dy / steps;
+		let x = x1;
+		let y = y1;
+
+		for (let i = 0; i <= steps; i++) {
+			point(x, y);
+			x += xIncrement;
+			y += yIncrement;
+		}
 	}
 }
 
 function draw() {
-	drawLine(50, 50, 350, 100);
-	stroke(0, 200, 0);
-	line(50, 55, 350, 105)
+	drawLine(0, 250, 500, 250);
+	drawLine(0, 500, 500, 0);
+	drawLine(0, 0, 500, 500);
+	drawLine(250, 0, 250, 500);
 }
