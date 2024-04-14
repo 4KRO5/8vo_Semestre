f1abf8d73b92fbc0e7f4ad9515f375d72756bf18
4KRO5 <4kro54k@gmail.com>
2024-04-13 22:32:23 -0700

    Corrección

--- Graficación y Multimedia/Bresenham/sketch.js
+++ Graficación y Multimedia/Bresenham/sketch.js
@@ -3,7 +3,7 @@ function setup() {
 	background(200);
 }
 
-function drawBresenhamLine(x1, y1, x2, y2) {
+function drawLine(x1, y1, x2, y2) {
 	let dx = Math.abs(x2 - x1);
 	let dy = Math.abs(y2 - y1);
 	let sx = (x1 < x2) ? 1 : -1;
@@ -25,8 +25,8 @@ function drawBresenhamLine(x1, y1, x2, y2) {
 }
 
 function draw() {
-	drawBresenhamLine(0, 250, 500, 250);
-	drawBresenhamLine(250, 0, 250, 500);
-	drawBresenhamLine(0, 0, 500, 500);
-	drawBresenhamLine(0, 500, 500, 0);
+	drawLine(0, 250, 500, 250);
+	drawLine(250, 0, 250, 500);
+	drawLine(0, 0, 500, 500);
+	drawLine(0, 500, 500, 0);
 }  
