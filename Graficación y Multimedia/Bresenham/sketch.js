17a590eca38cfa1408da2303c7362f844bac2001
4KRO5 <4kro54k@gmail.com>
2024-04-13 22:31:17 -0700

    Algoritmo Bresenham

---
+++ Graficación y Multimedia/Bresenham/sketch.js
@@ -0,0 +1,32 @@
+function setup() {
+	createCanvas(500, 500);
+	background(200);
+}
+
+function drawBresenhamLine(x1, y1, x2, y2) {
+	let dx = Math.abs(x2 - x1);
+	let dy = Math.abs(y2 - y1);
+	let sx = (x1 < x2) ? 1 : -1;
+	let sy = (y1 < y2) ? 1 : -1;
+	let err = dx - dy;
+
+	while (x1 !== x2 || y1 !== y2) {
+		point(x1, y1);
+		let e2 = 2 * err;
+		if (e2 > -dy) {
+			err -= dy;
+			x1 += sx;
+		}
+		if (e2 < dx) {
+			err += dx;
+			y1 += sy;
+		}
+	}
+}
+
+function draw() {
+	drawBresenhamLine(0, 250, 500, 250);
+	drawBresenhamLine(250, 0, 250, 500);
+	drawBresenhamLine(0, 0, 500, 500);
+	drawBresenhamLine(0, 500, 500, 0);
+}  
